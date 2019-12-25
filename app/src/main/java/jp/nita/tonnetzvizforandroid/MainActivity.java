fe9826c6b42a4db037d34a11032faa7c5af867e4
k-hatano <kent.ruffle.mgj626@gmail.com>
2019-12-26 04:45:48 +0900

    最低限動作するように修正

--- app/src/main/java/jp/nita/tonnetzvizforandroid/MainActivity.java
+++ app/src/main/java/jp/nita/tonnetzvizforandroid/MainActivity.java
@@ -1,14 +1,30 @@
 package jp.nita.tonnetzvizforandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.webkit.*;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView)findViewById(R.id.webView1);
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient( new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+
+            }
+        });
+
+        mWebView.loadUrl("file:///android_asset/tonnetz-viz/index.html");
     }
+
 }
