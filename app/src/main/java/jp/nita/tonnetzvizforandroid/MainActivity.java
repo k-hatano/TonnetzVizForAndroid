e9507ba182edbaf8aa6a49716f83d565a1b000c0
k-hatano <kent.ruffle.mgj626@gmail.com>
2020-01-01 20:34:46 +0900

    ファイルを選択できるようにまで

--- app/src/main/java/jp/nita/tonnetzvizforandroid/MainActivity.java
+++ app/src/main/java/jp/nita/tonnetzvizforandroid/MainActivity.java
@@ -1,7 +1,13 @@
 package jp.nita.tonnetzvizforandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.pm.PackageManager;
+import android.os.Environment;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -9,17 +15,29 @@ import android.webkit.*;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+
+import jp.nita.utils.FileSelectionDialog;
+
+import static jp.nita.utils.FileSelectionDialog.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
 
+    private static final int REQUEST_PERMISSION_READ_EXTERNAL_STORAGE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestReadExternalStoragePermission();
 
         initTabs();
 
@@ -80,6 +98,51 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
+        Button buttonSelectMidiFile = findViewById(R.id.button_select_midi_file);
+        buttonSelectMidiFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FileSelectionDialog dialog = new FileSelectionDialog(v.getContext(), new OnFileSelectListener() {
+                    @Override
+                    public void onFileSelect(File file) {
+                        ((TextView)findViewById(R.id.message)).setText(file.getAbsolutePath());
+                    }
+                });
+                File initialDirectory = Environment.getExternalStorageDirectory();
+
+                dialog.show(initialDirectory);
+            }
+        });
+
+    }
+
+    private void requestReadExternalStoragePermission()
+    {
+        if( PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission( this, Manifest.permission.READ_EXTERNAL_STORAGE ) )
+        {
+            return;
+        }
+        ActivityCompat.requestPermissions( this,
+                new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE },
+                REQUEST_PERMISSION_READ_EXTERNAL_STORAGE );
+    }
+
+    @Override
+    public void onRequestPermissionsResult( int requestCode, String[] permissions, int[] grantResults )
+    {
+        switch( requestCode )
+        {
+            case REQUEST_PERMISSION_READ_EXTERNAL_STORAGE:
+                if( grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED )
+                {
+                    Toast.makeText( this, "Permission denied.", Toast.LENGTH_SHORT ).show();
+                    finish();
+                    return;
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     protected void initTabs() {
---
+++ app/src/main/java/jp/nita/utils/OnFileSelectedListener.java
@@ -0,0 +1,21 @@
+//package org.wisterina.android.tool.dialogfragment.fileselect;
+package jp.nita.utils;
+
+import java.io.Serializable;
+
+
+/**
+ * ファイル選択のリスナーです。
+ */
+public interface OnFileSelectedListener extends Serializable {
+
+    /**
+     * ファイルが選択された時に呼び出されます。
+     * @param path パス
+     */
+    public abstract void onFileSelected(String path);
+    /**
+     * ファイル選択がキャンセルされたときに呼び出されます。
+     */
+    public abstract void onFileSelectCanceled();
+}
